05661514acdc2c9f5f36ea8ff093ed8a28c860a1
Ervin Cabascango <ervin.cabascango.s@gmail.com>
2020-04-05 17:09:47 -0500

    feat: read elements to LocalStorage

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,6 +15,12 @@ document.querySelector('#formulario').addEventListener('submit', addTweet);
 // Borrar Tweet 
 tweetList.addEventListener('click', deleteTweet);
 
+
+// Cargar Datos de LocalStorage
+document.addEventListener('DOMContentLoaded', localStorageListo);
+
+
+
 //Funciones
 
 //Añadir tweet al formulario
@@ -63,11 +69,8 @@ function addTweetLocalStorage(tweet) {
     tweets.push(tweet);
 
     // Convertir dato para LS
-     // Agregar al Local Storage
+    // Agregar al Local Storage
     localStorage.setItem('tweets', JSON.stringify(tweets));
-
-       
-
 }
 
 
@@ -81,3 +84,28 @@ function getTweetLocalStorage() {
     return tweets;
 }
 
+
+function localStorageListo() {
+    let tweets;
+    tweets = getTweetLocalStorage();
+
+    tweets.forEach(tweet => {
+        // Crear boton eliminar
+        const botonBorrar = document.createElement('a');
+        botonBorrar.classList = 'borrar-tweet';
+        botonBorrar.innerText = 'X';
+
+        //Crear elemento y añadir a la lista de tweets
+        const li = document.createElement('li');
+        li.innerText = tweet;
+        //Añade el boton de borrar
+        li.appendChild(botonBorrar);
+
+        //Agregamos el tweet a la lista
+        tweetList.appendChild(li);
+
+        //Agregar tweet a LocalStorage
+        addTweetLocalStorage(tweet);
+    });
+
+}
